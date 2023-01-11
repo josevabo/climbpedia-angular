888d6dc5ba8e14e9e85b4dfe328454fbf603cfe8
josevabo <43576654+josevabo@users.noreply.github.com>
2023-01-11 10:37:07 -0300

    deploy: environment.prod adjust for deploying in azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpointUrl: 'http://172.173.235.5/api',//'API_URL',//'http://localhost:8080/api',
+  authServiceUrl: 'http://172.173.235.5/auth/token'//'AUTH_URL',//'http://localhost:8080/auth/token'
 };
